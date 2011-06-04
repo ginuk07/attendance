dce903adf72eef3352c22f46264bba8a64c682d2
Ian Hilt <ian.hilt@gmail.com>
2011-06-03 21:30:48 -0400

    Order clients by name in attendance sheet
    This will help duplicates be more visible
    
    Signed-off-by: Ian Hilt <ian.hilt@gmail.com>

--- app/models/attendance_sheet.rb
+++ app/models/attendance_sheet.rb
@@ -1,6 +1,6 @@
 class AttendanceSheet < ActiveRecord::Base
   has_many :attendance_values, :dependent => :destroy
-  has_many :clients, :through => :attendance_values
+  has_many :clients, :through => :attendance_values, :order => :name
   accepts_nested_attributes_for :clients
   accepts_nested_attributes_for :attendance_values, :allow_destroy => true
 end
