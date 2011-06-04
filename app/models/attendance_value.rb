2cd39c8187d8ddfa4469a6f5e80a81b6b2ba49c9
Ian Hilt <ian.hilt@gmail.com>
2011-06-03 22:33:56 -0400

    Validate the presence of client.name and attendance_value.client
    Signed-off-by: Ian Hilt <ian.hilt@gmail.com>

--- app/models/attendance_value.rb
+++ app/models/attendance_value.rb
@@ -3,6 +3,7 @@ class AttendanceValue < ActiveRecord::Base
   belongs_to :attendance_sheet
   accepts_nested_attributes_for :client
   accepts_nested_attributes_for :attendance_sheet
+  validates_presence_of :client
   validates_each :client, :on => :create do |model, attr, value|
     AttendanceValue.joins(:attendance_sheet, :client).where(:client_id => 1).collect { |a| if a.attendance_sheet.date == model.attendance_sheet.date then model.errors.add(attr, 'already assigned to this attendance sheet') end }
   end
