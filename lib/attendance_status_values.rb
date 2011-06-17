ca4029b55489021a0cea4a2964ef9a7e4c8de501
Ian Hilt <ihilt@eastway.org>
2011-06-17 13:10:20 -0400

    Collect attendance status values into a module
    Signed-off-by: Ian Hilt <ihilt@eastway.org>

---
+++ lib/attendance_status_values.rb
@@ -0,0 +1,10 @@
+module AttendanceStatusValues
+  VALUES = [
+              "Present",
+              "Tardy",
+              "Unexcused Absence",
+              "Excused Absence",
+              "School Closed",
+              "Suspended"
+            ]
+end
